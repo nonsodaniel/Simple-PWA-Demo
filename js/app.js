ad0b766147e85db435a3c6c2571c8b701ec660cf
nonsodaniel <nonsodaniel07@gmail.com>
2021-06-22 03:52:54 +0100

    Integrated dynamic data using random user api

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
 
 
-
-// var jobs = []
-const BASE_URL = "https://randomuser.me/api/1.2/?results=10";
-const userList = document.querySelector(".jobs");
-// new Date('2010-06-06T13:49:48Z').toDateString()
+const BASE_URL = "https://randomuser.me/api/1.2/?results=15";
+const userList = document.querySelector(".users");
 
 const fetchUsers = () => {
   fetch(`${BASE_URL}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       if (data && data.results.length > 0) {
         let html = "";
         data.results.map((user) => {
@@ -19,45 +15,47 @@ const fetchUsers = () => {
             name: { title, first, last },
             email,
             phone,
-            dob: { age },
+            dob: { date, age },
             location: { street, city, state },
             picture: { medium, large },
             nat,
           } = user;
 
           html += `
-                <div class="job" id=${value}>
-                <div class="logo-wrap">
-                    
-                </div>
-                <h4 class="name">${title} ${first} ${last}</h4>
-                <div class="contact">
-                  <div class="email-wrap">
-                    <div class="title"><span><i class="fas fa-envelope-square"></i></span></div>
-                    <div class="email desc">${email}</div>
-                </div>
-                <div class="phone-wrap"> 
-                  <div class="title"><span><i class="fas fa-phone"></i></span></div>
-                  <div class="phone desc">${phone}</div>
-              </div>
-              <div class="dob-wrap">
-                <div class="title">Age</div>
-                <div class="phone desc">${age}</div>
+          <div class="user" id=${value}>
+          <div class="top">
+            <div class="photo-wrap">
+              <img src=${medium} alt=${`${first}-image`}>
             </div>
-              <div class="location-wrap">
-                <div class="title"><span><i class="fas fa-map-marker"></i></span></div>
-                <div class="phone desc">${street}, ${city}, ${state}.</div>
+            <h3 class="name">${title} ${first} ${last}</h3>
+            <div class="email">${email}</div>
+          </div>
+          <div class="contact">
+            <div class="phone-wrap">
+              <div class="title">Phone</div>
+              <div class="phone desc">${phone}</div>
+            </div>
+            <div class="dob-wrap">
+              <div class="title">Dob</div>
+              <div class="phone desc">${new Date(date).toDateString()}</div>
+            </div>
+            <div class="location-wrap">
+              <div class="title">Location</div>
+              <div class="phone desc">${street}, ${city}, ${state}.</div>
             </div>
             <div class="nat-wrap">
-              <div class="title"><span><i class="fas fa-flag"></i></span></div>
+              <div class="title">Nationality</div>
               <div class="phone desc">${nat}</div>
+            </div>
           </div>
-                </div>
-             
-          </div>
+        
+        </div>
                 `;
         });
-        console.log(html);
+        userList.innerHTML = html;
+      } else {
+        let html = "";
+        html = `<div>No Data Available</div>`;
         userList.innerHTML = html;
       }
     });
