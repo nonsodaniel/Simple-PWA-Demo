fd700f3aae4e76fdb1d4f4a7e5c2eb4647cd105b
nonsodaniel <nonsodaniel07@gmail.com>
2021-06-22 03:30:59 +0100

    Removed irrelevant files && restructured app

--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((reg) => console.log("service worker registered", reg))
-    .catch((err) => console.log("service worker not registered", err));
-}
+
 
 // var jobs = []
 const BASE_URL = "https://randomuser.me/api/1.2/?results=10";
--- sw.js
+++ sw.js
@@ -2,20 +2,21 @@ const staticCacheName = 'site-cache';
 const assets = [
   "/",
   "/index.html",
+  "/js/index.js",
   "/js/app.js",
-  '/css/styles.css',
-  '/css/font.css',
+  "/css/styles.css",
+  "/css/font.css",
+  "/manifest.json",
   "/img/logo.svg",
   "/img/msk192.png",
   "/img/msk512.png",
 ];
 
 // install event
-self.addEventListener('install', evt => {
-  //console.log('service worker installed');
+self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
+      console.log("caching shell assets");
       cache.addAll(assets);
     })
   );
